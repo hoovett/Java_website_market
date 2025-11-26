6b0cb01bcd3b466bc5d9da8c472ba47e54734191
hoovett <hoovett@gmail.com>
2025-11-26 12:05:02 +0100

    finished ExceptionHandler

--- src/main/java/com/example/First_website/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/First_website/Exceptions/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.example.First_website.Exceptions;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.hibernate.boot.model.naming.IllegalIdentifierException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -36,12 +38,65 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleValidationErrors(MethodArgumentNotValidException ex)
+    public ResponseEntity<ErrorResponse> handleValidationErrors(MethodArgumentNotValidException ex, HttpServletRequest request)
     {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getFieldErrors().forEach(error ->
                 errors.put(error.getField(), error.getDefaultMessage()));
-        return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+
+        ErrorResponse response = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                "Validation failed",
+                request.getRequestURI(),
+                errors
+        );
+
+        return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleAllException(Exception ex, HttpServletRequest request)
+    {
+        log.error("Unexpected error: {}", ex.getMessage());
+
+        ErrorResponse response  = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+
+    @ExceptionHandler(IllegalIdentifierException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgument(IllegalIdentifierException ex, HttpServletRequest request)
+    {
+        ErrorResponse response = new ErrorResponse(
+                LocalDateTime.now(),
+                400,
+                "Bad Request",
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(org.springframework.dao.DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateKey(DataIntegrityViolationException ex, HttpServletRequest request)
+    {
+        ErrorResponse response = new ErrorResponse(
+                LocalDateTime.now(),
+                409,
+                "Conflict",
+                "This value already exists in database",
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+    }
 }
